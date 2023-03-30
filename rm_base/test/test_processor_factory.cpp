c4d33c4a4426d781ba51d68f5fc8cc1cd21950e7
冯云龙 <ericc_feng@qq.com>
2023-03-30 15:24:13 +0800

    feat: Add gtest factory

---
+++ rm_base/test/test_processor_factory.cpp
@@ -0,0 +1,64 @@
+// Copyright 2023 Yunlong Feng
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <rm_base/buffer_processor_factory.hpp>
+
+namespace rm_base
+{
+
+class TestProcessor : public ProcessInterface
+{
+public:
+  TestProcessor(rclcpp::Node * node)
+  : ProcessInterface(node)
+  {
+  }
+
+  bool process_packet(const Packet & packet) override
+  {
+    (void) packet;
+    return true;
+  }
+};
+
+PROCESSOR_REGISTER(TestProcessor, 0xff)
+
+} // namespace rm_bae
+
+
+TEST(TestProcessorFactory, register_class)
+{
+  EXPECT_TRUE(rm_base::register_plugin_TestProcessor.is_registed);
+}
+
+TEST(TestProcessorFactory, create_class)
+{
+  rclcpp::init(0, nullptr);
+  auto node = std::make_shared<rclcpp::Node>("test_node");
+  EXPECT_TRUE(rm_base::ProcessFactory::create(0xff, node.get()));
+  rclcpp::shutdown();
+}
+
+TEST(TestProcessorFactory, process_packet)
+{
+  rclcpp::init(0, nullptr);
+  auto node = std::make_shared<rclcpp::Node>("test_node");
+  rm_base::ProcessFactory::create(0xff, node.get());
+  EXPECT_TRUE(rm_base::ProcessFactory::process_packet(0xff, rmoss_base::FixedPacket16()));
+  rclcpp::shutdown();
+}
