b5e71bb33b049786df4955cf6003fd6e7663d82a
陈霆旭 <915800506@qq.com>
2023-04-09 18:11:09 +0800

    Create power heat data processor.

---
+++ rm_base/src/buffer_processor/power_heat_data_processor.cpp
@@ -0,0 +1,77 @@
+// Copyright 2023 Tingxu Chen
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "rm_base/buffer_processor_factory.hpp"
+
+#include <rm_base/protocol_types.hpp>
+#include "rm_interfaces/msg/power_heat_data.hpp"
+
+namespace rm_base
+{
+
+typedef struct __packed
+{
+  uint16_t chassis_volt;
+  uint16_t chassis_current;
+  float chassis_power;
+  uint16_t chassis_power_buffer;
+  uint16_t shooter_id1_17mm_cooling_heat;
+  uint16_t shooter_id2_17mm_cooling_heat;
+  uint16_t shooter_id1_42mm_cooling_heat;
+} __attribute__((__packed__)) ext_power_heat_data_t;
+
+class PowerHeatDataProcessor : public ProcessInterface
+{
+public:
+  explicit PowerHeatDataProcessor(rclcpp::Node * node)
+  : ProcessInterface(node)
+  {
+    auto topic_name = node_->declare_parameter("power_heat_data_topic", "power_heat_data");
+    RCLCPP_INFO(node_->get_logger(), "power_heat_data_topic: %s", topic_name.c_str());
+    pub_ = node_->create_publisher<rm_interfaces::msg::PowerHeatData>(topic_name, 10);
+  }
+
+  bool process_packet(const Packet & packet)
+  {
+    if (std::holds_alternative<rmoss_base::FixedPacket64>(packet)) {
+      auto packet_recv = std::get<rmoss_base::FixedPacket64>(packet);
+      rm_interfaces::msg::PowerHeatData::UniquePtr msg(new rm_interfaces::msg::PowerHeatData());
+
+      ext_power_heat_data_t data;
+      packet_recv.unload_data(data, 2);
+      msg->chassis_volt = data.chassis_volt;
+      msg->chassis_current = data.chassis_current;
+      msg->chassis_power = data.chassis_power;
+      msg->chassis_power_buffer = data.chassis_power_buffer;
+      msg->shooter_id1_17mm_cooling_heat = data.shooter_id1_17mm_cooling_heat;
+      msg->shooter_id2_17mm_cooling_heat = data.shooter_id2_17mm_cooling_heat;
+      msg->shooter_id1_42mm_cooling_heat = data.shooter_id1_42mm_cooling_heat;
+
+      pub_->publish(std::move(msg));
+      return true;
+    } else {
+      RCLCPP_WARN(node_->get_logger(), "Invalid length of data frame for PowerHeatData processor.");
+      return false;
+    }
+  }
+
+private:
+  rclcpp::Publisher<rm_interfaces::msg::PowerHeatData>::SharedPtr pub_;
+};
+
+#include <rm_base/register_macro.hpp>
+
+REGISTER_PROCESSOR_CLASS(PowerHeatDataProcessor, static_cast<uint8_t>(RecvID::POWERHEATDATA))
+
+}  // namespace rm_base
